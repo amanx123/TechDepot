ac5fb41f28019fa150f13ce6214a0cc2384390da
aman <amanagarwalx123@gmail.com>
2023-07-09 19:51:01 +0530

    initial

---
+++ client/src/data.js
@@ -0,0 +1,46 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+      title: "YEAR END SALE",
+      desc: "Get the lowest prices on gadgets in the online market",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1611078489935-0cb964de46d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
+      title: "LAPTOPS AT BEST PRICES",
+      desc: "GET UPTO 40% OFF",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1585068247241-753a57d51cf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2908&q=80",
+      title: "Mobiles",
+      desc: "Get Smartphones at best prices with exchange offers.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      title: "HEADPHONES",
+      cat:"headphones"
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1624823183493-ed5832f48f18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+      title: "ACCESSORIES",
+      cat:"accessories"
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1592839719941-8e2651039d01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1125&q=80",
+      title: "Consoles",
+      cat:"gaming"
+    },
+  ];
+
+  
